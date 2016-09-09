a1bb639e75ff77f33e1d74cb605bf327c4b0ff33
Matthew Reyes <mreyez@gmail.com>
2016-09-09 08:19:23 -0700

    Add Player and Team models

---
+++ Fantasy-Football/src/Fantasy-Football/Models/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fantasy_Football.Models
+{
+    public class Player
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public int TeamId { get; set; }
+        public virtual Team Team { get; set; }
+    }
+}
---
+++ Fantasy-Football/src/Fantasy-Football/Models/Team.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fantasy_Football.Models
+{
+    public class Team
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Player> Players { get; set; }
+        public int ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+
+    }
+}
