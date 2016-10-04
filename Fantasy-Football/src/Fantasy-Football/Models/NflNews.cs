669dade8db44adc12ced5922f8d36cb99227afc8
Matthew Reyes <mreyez@gmail.com>
2016-10-04 15:17:03 -0700

    add nfl team images and finish styling news block items

--- Fantasy-Football/src/Fantasy-Football/Models/NflNews.cs
+++ Fantasy-Football/src/Fantasy-Football/Models/NflNews.cs
@@ -20,13 +20,35 @@ namespace Fantasy_Football.Models
         public string Link { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }
+        public string Opinion { get; set; }
+        public string Team { get; set; }
         public NflNews() { }
+        public NflNews(string title, string description, string opinion, string team, string link, DateTime date)
+        {
+            Title = title;
+            Description = description;
+            Link = link;
+            Date = date;
+            Opinion = opinion;
+            Team = team;
+        }
+        public NflNews(string title, string description, string team, string link, DateTime date)
+        {
+            Title = title;
+            Description = description;
+            Link = link;
+            Date = date;
+            Team = team;
+            Opinion = "";
+        }
         public NflNews(string title, string description, string link, DateTime date)
         {
             Title = title;
             Description = description;
             Link = link;
             Date = date;
+            Team = "";
+            Opinion = "";
         }
         public static async Task<List<NflNews>> GetFeeds()
         {
@@ -55,8 +77,53 @@ namespace Fantasy_Football.Models
                     foreach (var i in items)
                     {
                         var newDescription = i.description.Replace("&quot;", @"""");
-                        NflNews newNews = new NflNews(i.title, newDescription, i.link, Convert.ToDateTime(i.date));
-                        news.Add(newNews);
+                        
+                        var phrases = new List<String>();
+                        var newsTeam = new List<String>();
+                        var stringLength = newDescription.Length;
+                        int start = 0;
+                        int position = 0;
+                        int teamPosition = 0;
+
+                        //Find the team name.  Used to display an image
+                        do
+                        {
+                            teamPosition = i.title.IndexOf('|', start);
+                            if (teamPosition >= 0)
+                            {
+                                newsTeam.Add(i.title.Substring(start, teamPosition - 1).Trim());
+                                teamPosition = -1;
+                            }
+                            else
+                            {
+                                newsTeam.Add("");
+                            }
+                        } while (teamPosition >= 0);
+
+                        //split the description if there is a Footballguys take on the news.
+                        do
+                        {
+                            position = newDescription.IndexOf("Footballguys", start);
+                            if (position >= 0)
+                            {
+                                phrases.Add(newDescription.Substring(start, position - 1).Trim());
+                                phrases.Add(newDescription.Substring(position).Trim());
+                                position = -1;
+                            }
+                        } while (position > 0);
+
+                        //check phrases array to see if the description was split.
+                        if(phrases.Count < 1)
+                        {
+                            NflNews newNews = new NflNews(i.title, newDescription, newsTeam[0], i.link, Convert.ToDateTime(i.date));
+                            news.Add(newNews);
+                        }
+                        else
+                        {
+                            NflNews newNews = new NflNews(i.title, phrases[0],phrases[1],newsTeam[0], i.link, Convert.ToDateTime(i.date));
+                            news.Add(newNews);
+                        }
+
                     }
                 }
 
