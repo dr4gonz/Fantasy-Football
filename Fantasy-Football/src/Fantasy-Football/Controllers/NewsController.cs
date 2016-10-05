362d8050dc609e2264ccb8e283111d9d5a353be6
Matthew Reyes <mreyez@gmail.com>
2016-10-05 10:47:46 -0700

    make only 5 news items on main page with linke to news page

---
+++ Fantasy-Football/src/Fantasy-Football/Controllers/NewsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Fantasy_Football.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Fantasy_Football.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            var news = await NflNews.GetFeeds();
+            return View(news);
+        }
+    }
+}
